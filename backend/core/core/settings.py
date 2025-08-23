74c0ad1ad62ac7e6279d0153d91b0831b9579ddd
uikeytulsidas <uikeytulsidas@gmail.com>
2025-08-23 18:19:51 +0530

    Update settings.py

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-GROQ_API_KEY='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' 
+GROQ_API_KEY=' ' 
 # Replace  your  actual Groq API key here 
 
 SECRET_KEY = config("SECRET_KEY", default="unsafe-secret")
